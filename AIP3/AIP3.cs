909082bcf5b33348aee5b8e3dd37b82c11e051aa
Matmun <47967980+Matmun@users.noreply.github.com>
2021-05-23 17:32:28 +0200

    +Projekt 3 bez biblioteki

---
+++ AIP3/AIP3.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ML.Probabilistic.Math;
+using Microsoft.ML.Probabilistic.Models;
+using System;
+
+namespace AIP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10]; //number of throws
+
+            for (int i = 0; i < data.Length; i++)
+                data[i] = Rand.Binomial(10, 0.5);
+            Variable<double> a = Variable.Beta(1, 1);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Variable<int> x = Variable.Binomial(10, a);
+                x.ObservedValue = data[i];
+            }
+
+            InferenceEngine engine = new InferenceEngine();// Retrieve the posterior distributions  
+            Console.WriteLine("resoult=" + engine.Infer(a));
+        }
+    }
+}
